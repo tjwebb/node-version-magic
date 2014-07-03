afaa76347a1193705617f4960b680fc7402aad76
Travis Webb <me@traviswebb.com>
2014-07-03 05:02:18 -0400

    basic tests passing; compiled nvr coffeescript

--- index.js
+++ index.js
@@ -11,28 +11,31 @@ var child = require('child_process');
 var n = require('n-api');
 var semver = require('semver');
 var Resolver = require('node-version-resolver');
-var pkg = require(path.resolve(process.cwd(), 'package.json'));
 
-if (!pkg.engines || !pkg.engines.node) {
-  log.warn(pkg.name, 'I can\'t validate the node version if your engines.node is empty in package.json');
-  return;
-}
+exports.enforce = function (pkg) {
 
-if (semver.satisfies(process.version, pkg.engines.node)) {
-  log.info(pkg.name, 'node version', process.version, 'satisfies', pkg.engines.node);
-  return;
-}
+  if (!pkg.engines || !pkg.engines.node) {
+    log.warn(pkg.name, 'I can\'t validate the node version if your engines.node is empty in pkg.json');
+    return;
+  }
 
-var r = new Resolver();
-var required = r(pkg.engines.node).satisfy(process.version, r.latest_stable);
+  if (semver.satisfies(process.version, pkg.engines.node)) {
+    log.info(pkg.name, 'node version', process.version, 'satisfies', pkg.engines.node);
+    return;
+  }
 
-log.info(pkg.name, 'needs node', required);
-if (!n.bin(required)) {
-  log.http(required, 'installing...');
-  n(required);
-  n(process.version);
-}
+  var r = new Resolver(function () {
+    var required = r(pkg.engines.node).satisfy(process.version, r.latest_stable);
 
-log.info(pkg.name, 'running with node', required);
-n.use.sync(required, process.argv.join(' '));
-process.exit(0);
+    log.info(pkg.name, 'needs node', required);
+    if (!n.bin(required)) {
+      log.http(required, 'installing...');
+      n(required);
+      n(process.version);
+    }
+
+    log.info(pkg.name, 'running with node', required);
+    n.use.sync(required, process.argv.join(' '));
+    process.exit(0);
+  });
+};
--- spec/index.js
+++ spec/index.js
@@ -2,6 +2,27 @@ global.log = require('npmlog');
 log.heading = 'node-version';
 log.level = 'verbose';
 
+var assert = require('chai').assert;
+
+describe('node-version-resolver', function () {
+  var Resolver = require('../resolver');
+
+  it('should do its thing', function (done) {
+    var r = new Resolver(function () {
+      assert.equal(r.satisfy('>=0.8.0 <0.9'), '0.8.27');
+      assert.equal(r.satisfy('=0.11.13'), '0.11.13');
+      done();
+    });
+  });
+
+});
+
 describe('node-version', function () {
+
+  describe('#enforce', function () {
+    it('should validate this process', function () {
+      //assert.
+    });
+  });
 
 });
